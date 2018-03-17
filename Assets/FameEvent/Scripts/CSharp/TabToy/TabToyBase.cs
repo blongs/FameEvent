9980f6e4c1af61bb52408c530906dc032be31ad3
PCOS-1709171206\Administrator <465823023@qq.com>
2018-03-17 18:52:08 +0800

    toaboy

---
+++ Assets/FameEvent/Scripts/CSharp/TabToy/TabToyBase.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TabToyBase : MonoBase
+{
+    public ushort[] msgIds;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+    public void RegistSelf(MonoBase mono, params ushort[] msgs)
+    {
+        TabToyManager.Instance.RegistMsg(mono, msgs);
+    }
+
+    public void UnRegistSelf(MonoBase mono, params ushort[] msgs)
+    {
+        TabToyManager.Instance.UnRegistMsg(mono, msgs);
+    }
+
+    public void SendMsg(MsgBase msg)
+    {
+        TabToyManager.Instance.SendMsg(msg);
+    }
+
+    private void OnDestroy()
+    {
+        if (msgIds != null)
+        {
+            UnRegistSelf(this, msgIds);
+        }
+    }
+}
---
+++ Assets/FameEvent/Scripts/CSharp/TabToy/TableTestLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableTestLoader : TabToyBase {
+    public override void ProcessEvent(MsgBase tmpMsg)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
